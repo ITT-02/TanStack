f5929d87aeed9088df73c528547b10e985f7d4b0
ITT-02 <itintayattito@gmail.com>
2026-04-22 22:24:04 -0500

    Se agrego MUI

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,22 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+
 import {App} from './App'
+import { theme } from './styles/theme';
 
-import {QueryClient, QueryClientProvider} from '@tanstack/react-query'
-
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
+   <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
     </QueryClientProvider>
   </StrictMode>,
 )
